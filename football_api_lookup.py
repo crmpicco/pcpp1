32ad6e5b9ab7d58378591c50e0442ee0aeb3f0fe
Craig R Morton <crmpicco@aol.co.uk>
2025-01-31 17:29:39 +0800

    Tabs -> Spaces
    Fixing pycodestyle warnings (W292)

--- football_api_lookup.py
+++ football_api_lookup.py
@@ -1,18 +1,15 @@
 import requests
 
 def get_rangers_players(football_api_key):
-	url = "https://v3.football.api-sports.io/fixtures"
+    url = "https://v3.football.api-sports.io/fixtures"
 
-	# the free tier of the API is restricted to the season 2021 onwards
-	querystring = {"team":"257","season":"2021"}
+    # the free tier of the API is restricted to the season 2021 onwards
+    querystring = {"team":"257","season":"2021"}
+    headers = {
+        "x-rapidapi-key": football_api_key,
+        "x-rapidapi-host": "v3.football.api-sports.io"
+    }
 
-	headers = {
-		"x-rapidapi-key": football_api_key,
-		"x-rapidapi-host": "v3.football.api-sports.io"
-	}
-
-	# response = requests.get(url, headers=headers, params=querystring)
-
-	print(response.json())
-
-	return response.json()
+    # response = requests.get(url, headers=headers, params=querystring)
+    print(response.json())
+    return response.json()
